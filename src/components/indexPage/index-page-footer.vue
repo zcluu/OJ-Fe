9428bf957219b56b01624f9f4f216487000ea66e
DMSintalor <1256234567@qq.com>
2023-06-24 00:03:59 +0800

    add basic information

---
+++ src/components/indexPage/index-page-footer.vue
@@ -0,0 +1,60 @@
+<script>
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+    name: "index-page-footer"
+})
+</script>
+
+<template>
+    <div class="footer-box">
+        <div style="margin-bottom: 10px;">
+            <el-text class="footer-title">
+                Link
+            </el-text>
+        </div>
+        <el-row>
+            <el-col :span="8">
+                <el-space direction="vertical" alignment="start">
+                    <el-link href="https://www.sdufe.edu.cn/">SDUFE</el-link>
+                    <el-link href="https://github.com/sdufeACM">GitHub</el-link>
+                    <el-link href="https://github.com/QingdaoU/OnlineJudge">QDU OnlineJudge</el-link>
+                </el-space>
+            </el-col>
+            <el-col :span="8" style="position: relative;">
+            </el-col>
+            <el-col :span="8"></el-col>
+        </el-row>
+        <el-text style="font-weight: bold;">
+            © 2023-2023 SDUFEACM && <a href="http://cslzc.py00.top/#/" style="color: #fff;">CSLZC</a>.
+            All Rights Reserved.
+        </el-text>
+    </div>
+</template>
+
+<style scoped lang="scss">
+.footer-box {
+  //background-color: rgba(239, 239, 239, 0.65);
+  background: url("http://oss.py00.cn/oj/footer-bg.jpg");
+  height: 100%;
+  width: calc(100% - 40px);
+  padding: 20px;
+  color: #fff;
+
+  .el-link, .el-text {
+    --el-link-text-color: #fff !important;
+    --el-text-color: #fff !important;
+  }
+
+  .footer-title {
+    font-weight: bold;
+    font-size: 18px;
+  }
+
+  a {
+    &:hover {
+      color: var(--el-color-primary);
+    }
+  }
+}
+</style>
---
+++ src/components/indexPage/index-page-header.vue
@@ -0,0 +1,77 @@
+<script>
+import {defineComponent} from 'vue'
+import {Grid, HomeFilled} from "@element-plus/icons-vue";
+
+export default defineComponent({
+    name: "main-page-header",
+    components: {Grid, HomeFilled}
+})
+</script>
+<script setup>
+import {List} from "@element-plus/icons-vue";
+
+let activeIndex = '/' + window.location.pathname.slice(1).split('/')[0]
+</script>
+<template>
+    <el-menu
+            :default-active="activeIndex"
+            mode="horizontal"
+            :ellipsis="false"
+            background-color="#fff"
+            active-text-color="rgb(0, 97, 174)"
+            :router="true"
+            style="box-shadow: 0 0 5px #aaa"
+    >
+        <el-menu-item class="logo">
+            <el-image src="http://oss.py00.cn/oj/sdufeacm.jpg"
+                      style="height: 40px;padding-top: 10px;"
+            ></el-image>
+        </el-menu-item>
+        <div style="flex: 1;"></div>
+        <el-menu-item index="/home">
+            <el-icon>
+                <HomeFilled/>
+            </el-icon>
+            Home
+        </el-menu-item>
+        <el-menu-item index="/problems">
+            <el-icon>
+                <Grid/>
+            </el-icon>
+            Problems
+        </el-menu-item>
+        <el-menu-item index="/contest">
+            <el-icon>
+                <List/>
+            </el-icon>
+            Contests
+        </el-menu-item>
+        <div style="padding: 0 5px;"></div>
+    </el-menu>
+</template>
+
+<style scoped lang="scss">
+.el-menu-item {
+  --el-menu-hover-text-color: var(--sdufe-color);
+  --el-menu-hover-bg-color: #ffffff;
+
+  &:focus {
+    background-color: transparent !important;
+    border-bottom-color: var(--sdufe-color);
+  }
+
+  &:hover {
+    background-color: transparent !important;
+    border-bottom-color: var(--sdufe-color);
+    cursor: pointer;
+  }
+}
+
+.logo {
+  &:hover {
+    background-color: transparent !important;
+    border-bottom-color: transparent;
+    cursor: default;
+  }
+}
+</style>
