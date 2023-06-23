22d6046b5f7f6c22bb878e751865a22bf9d09380
DMSintalor <1256234567@qq.com>
2023-06-24 00:03:01 +0800

    modify router

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,36 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import {createRouter, createWebHistory} from 'vue-router'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+            path: '/',
+            name: 'index',
+            component: () => import('@/views/indexPage.vue'),
+            children: [
+                {
+                    path: 'home',
+                    name: 'home',
+                    component: () => import('@/views/homePage.vue')
+                },
+                {
+                    path: 'problems',
+                    name: 'problems',
+                    component: () => import('@/views/problemPage.vue')
+                },
+                {
+                    path: 'contest',
+                    name: 'contest',
+                    component: () => import('@/views/contestPage.vue'),
+                    children:[{
+                        path:':cid',
+                        name:'contestDetail',
+                        component:()=>import('@/views/contestPage.vue')
+                    }]
+                },
+            ]
+        },
+    ]
 })
 
 export default router
