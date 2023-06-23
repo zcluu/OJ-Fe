6cf57a2afc90bac38c3741fb9dd9995637fe49bf
DMSintalor <1256234567@qq.com>
2023-06-24 00:03:31 +0800

    add basic contest page

---
+++ src/views/contestDetail.vue
@@ -0,0 +1,15 @@
+<script>
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+    name: "contestDetail"
+})
+</script>
+
+<template>
+contestDetail
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/views/contestPage.vue
@@ -0,0 +1,15 @@
+<script>
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+    name: "contestPage"
+})
+</script>
+
+<template>
+  <router-view/>
+</template>
+
+<style scoped>
+
+</style>
