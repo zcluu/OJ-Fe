295e46430631436f2dfee8baf63c9114ae5bf8c6
DMSintalor <1256234567@qq.com>
2023-06-25 23:33:28 +0800

    add animation

--- src/components/userController.vue
+++ src/components/userController.vue
@@ -9,37 +9,70 @@ export default defineComponent({
 </script>
 
 <template>
-    <div class="main-box">
-        <el-link href="login" :underline="false">
-            登录
-        </el-link>
-        <el-link href="register" :underline="false">
-            注册
-        </el-link>
-        <a href="#">
+    <div class="outer-box">
+        <div class="inner-box">
             <el-avatar
-                    style="background-color: #fff;"
+                    style="background-color: transparent;"
                     src="http://oss.py00.cn/blog/avatar.png"
                     :size="30"
             ></el-avatar>
-        </a>
+            <el-link href="login" class="hidden" :underline="false">
+                登录
+            </el-link>
+            <el-link href="register" class="hidden" :underline="false">
+                注册
+            </el-link>
+        </div>
     </div>
-
 </template>
 
 <style scoped lang="scss">
+.outer-box {
+  z-index: 999;
+  position: absolute;
+  right: 0;
+  top: 80px;
+  width: 40px;
+
+  border-radius: 5px 0 0 5px;
+
+  transition-duration: .3s;
+  overflow: hidden;
+
+  .inner-box {
+    width: 120px;
+    height: 40px;
+    background-color: #dedede;
+    color: #fff;
+
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    text-align: center;
+  }
+
+  &:hover {
+    width: 120px;
+  }
+}
+
 .main-box {
+  z-index: 100;
   width: 120px;
+  overflow: hidden;
   display: flex;
   justify-content: space-around;
   align-items: center;
   text-align: center;
-  border-radius: 0 0 0 5px;
-  background-color: rgba(204, 204, 204, 0.37);
+  border-radius: 5px 0 0 5px;
+  background-color: #fafafa;
   height: 40px;
+  transition-duration: .5s;
+  box-shadow: 0 0 5px #ccc;
 
-    position: absolute;
-    right: 0;
-    top: 58px;
+  position: absolute;
+  right: -80px;
+  top: 70px;
+
 }
 </style>
