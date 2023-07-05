2d2cb3700a8d0f2c112c059c6e349ce02faf30e6
DMSintalor <1256234567@qq.com>
2023-07-06 00:03:20 +0800

    add some routes

--- src/router/index.js
+++ src/router/index.js
@@ -10,6 +10,15 @@ const router = createRouter({
             component: () => import('@/views/indexPage.vue'),
             children: [
                 {
+                    path:'login',
+                    name:'userLogin',
+                    component:()=>import('@/views/user/loginPage.vue')
+                },
+                {
+                    path:'register',
+                    component:()=>import('@/views/user/registerPage.vue')
+                },
+                {
                     path: 'home',
                     name: 'home',
                     component: () => import('@/views/home/homePage.vue')
@@ -22,7 +31,19 @@ const router = createRouter({
                 {
                     path: '/problem/detail/:pid',
                     name: 'problemDetail',
-                    component: () => import('@/views/problem/problemDetail.vue')
+                    component: () => import('@/views/problem/problemDetail.vue'),
+                    children:[
+                        {
+                            path: '',
+                            name: 'problemDescription',
+                            component:()=>import('@/components/problemPage/problem-description.vue')
+                        },
+                        {
+                            path: 'submissions',
+                            name: 'problemSubmission',
+                            component:()=>import('@/components/problemPage/problem-submission.vue')
+                        }
+                    ]
                 },
                 {
                     path: 'contest/:pid?',
@@ -32,7 +53,52 @@ const router = createRouter({
                 {
                     path: 'contest/detail/:cid',
                     name: 'contestDetail',
-                    component: () => import('@/views/contest/contestDetail.vue')
+                    component: () => import('@/views/contest/contestDetail.vue'),
+                    children: [
+                        {
+                            path: '',
+                            component: () => import('@/components/contestPage/overview.vue'),
+                            children: [
+                                {
+                                    path: '',
+                                    name: 'contestDescription',
+                                    component: () => import('@/components/contestPage/description.vue')
+                                },
+                                {
+                                    path: 'problems',
+                                    name: 'contestProblems',
+                                    component: () => import('@/components/contestPage/problems.vue'),
+                                },
+                                {
+                                    path: 'rank',
+                                    name: 'contestRank',
+                                    component: () => import('@/components/contestPage/rank.vue'),
+                                },
+                                {
+                                    path: 'announcement',
+                                    name: 'contestAnnouncements',
+                                    component: () => import('@/components/contestPage/announcement.vue'),
+                                }
+                            ]
+                        },
+                        {
+                            path: 'problems/:sid',
+                            name: 'contestProblemDetail',
+                            component: () => import('@/views/contest/contestProblemDetail.vue'),
+                            children:[
+                                {
+                                    path: '',
+                                    name: 'contestProblemDescription',
+                                    component:()=>import('@/components/contestPage/problem/problem-description.vue')
+                                },
+                                {
+                                    path: 'submissions',
+                                    name: 'contestProblemSubmission',
+                                    component:()=>import('@/components/contestPage/problem/problem-submission.vue')
+                                }
+                            ]
+                        },
+                    ]
                 },
                 {
                     path: 'announcement',
@@ -42,7 +108,7 @@ const router = createRouter({
                 {
                     path: 'announcement/:aid',
                     name: 'announcementDetail',
-                    component: () => import('@/views/contest/announcementDetail.vue')
+                    component: () => import('@/views/announcement/announcementPage.vue')
                 },
             ]
         }
