688257b79e8e655ba48e28b3a4473a6a80d11e91
DMSintalor <1256234567@qq.com>
2023-07-08 13:40:41 +0800

    add admin page

---
+++ src/views/adminPage.vue
@@ -0,0 +1,15 @@
+<script>
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+    name: "adminPage"
+})
+</script>
+
+<template>
+
+</template>
+
+<style scoped lang="scss">
+
+</style>
