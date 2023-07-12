02cfaa07c0c8bb08effd2eec2e88325abddc49cd
DMSintalor <1256234567@qq.com>
2023-07-12 22:57:57 +0800

    add 404 html

---
+++ src/views/404_not_found.vue
@@ -0,0 +1,15 @@
+<script>
+import {defineComponent} from 'vue'
+import img from '@/assets/404bg.jpg'
+
+export default defineComponent({
+    name: "404",
+    beforeCreate() {
+        window.location.href = '/404.html'
+    }
+})
+</script>
+
+<template>
+</template>
+
